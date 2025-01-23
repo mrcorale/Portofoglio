97b99bdbe92f5cfaf779bb6f579c50052032d3d9
Badan Alexandru <52911474+mrcorale@users.noreply.github.com>
2025-01-23 21:49:50 +0200

    Add files via upload

---
+++ BadanAlexandru_files/js/site.js
@@ -0,0 +1,31 @@
+﻿// smooth scroll
+$(document).ready(function(){
+    $(".navbar .nav-link").on('click', function(event) {
+
+        if (this.hash !== "") {
+
+            event.preventDefault();
+
+            var hash = this.hash;
+
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function(){
+                window.location.hash = hash;
+            });
+        } 
+    });
+});
+
+// navbar toggle
+$('#nav-toggle').click(function(){
+    $(this).toggleClass('is-active')
+    $('ul.nav').toggleClass('show');
+});
+//----------------------------------------------------------------------------------------------------------------------------------//
+
+AOS.init({
+    duration: 1000,
+    endEvent: scroll,
+    once: false
+});
